48b3eb04bc895142af7cb8573200994f3a08b4a2
ApiAbuse <68893555+ixzy@users.noreply.github.com>
2022-12-18 15:10:21 -0500

    Create checker.py

---
+++ checker.py
@@ -0,0 +1,9 @@
+import requests
+
+BOTS = ["http://192.168.1.35:4200","https://lol.lol/"] # EXAMPLE bots
+for i in range(len(BOTS)):
+ r=requests.get(BOTS[i])
+ if r == "pong":
+    print(f"{i} bot(s) online...")
+    if i > len(BOTS):
+        break
