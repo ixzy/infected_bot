9540b59874d25518180001ff0dc9b67ac5b611e3
ApiAbuse <68893555+ixzy@users.noreply.github.com>
2023-03-17 22:11:05 -0400

    Update checker.py

--- checker.py
+++ checker.py
@@ -4,7 +4,7 @@ BOTS = ["http://192.168.1.35:4200","https://lol.lol/"] # EXAMPLE bots
 WORKING_BOTS = []
 for i in range(len(BOTS)):
  r=requests.get(BOTS[i])
- if r == "pong":
+ if r.text == "pong":
     print(f"{i} bot(s) online... [{BOTS[i]}]"  + WORKING_BOTS.extend[BOTS[i]])
     if i > len(BOTS):
         input('done')
